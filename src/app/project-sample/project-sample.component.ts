c2009baed379c080794237e50a6ed1c6a0d95d07
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-06-28 22:49:48 +0200

    component injected with variables

--- src/app/project-sample/project-sample.component.ts
+++ src/app/project-sample/project-sample.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project-sample',
   templateUrl: './project-sample.component.html',
   styleUrls: ['./project-sample.component.scss']
 })
-export class ProjectSampleComponent {
-  @Input() img: string ='';
-  @Input() skills: String  ='';
-  @Input() description: string ='';
-  @Input() text: string ='';
+export class ProjectSampleComponent implements OnInit {
+  @Input() img: string;
+  @Input() skills: String;
+  @Input() description: string;
+  @Input() title: string;
+
+  ngOnInit(): void {
+    console.log(this.img,this.skills);
+  }
 
 
 }
