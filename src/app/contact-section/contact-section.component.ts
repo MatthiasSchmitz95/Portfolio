280da3c4ede48e7dfcf50f2c18328e4b4b8683a4
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-07-02 15:25:12 +0200

    styled form, added validation

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -12,4 +12,24 @@ export class ContactSectionComponent {
     });
   }
 
+  inputCheck(inputId:string,errId:string){
+    console.log(inputId);
+    let check = document.getElementById(inputId) as HTMLInputElement;
+    let err = document.getElementById(errId);
+    const isValid = check.checkValidity();
+    
+    if (check.value.length > 2 && isValid) {
+      check.classList.remove('error');
+      check.classList.add('viable');
+      err.style.display = 'none';
+    }
+
+    else{
+      check.classList.remove('viable');
+      check.classList.add('error');
+      err.style.display = 'block';
+    }
+
+  }
+
 }
