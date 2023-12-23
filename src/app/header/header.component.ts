bce05ead121ab6a2282faf53faedf497b3308cee
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-12-23 20:09:57 +0100

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -42,7 +42,6 @@ export class HeaderComponent {
 
   addUnderline(spanId) {
     const spanElements = ['about', 'skills', 'portfolio', 'contact'];
-    ('addUnderline called for spanId:', spanId);
     spanElements.forEach((element) => {
       const span = document.getElementById('span-' + element);
       if (span) {
